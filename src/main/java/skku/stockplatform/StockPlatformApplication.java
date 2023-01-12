6765be7bf08df6c2f535856d53b0ee0b23e33302
Nam-gu <skarn2158@naver.com>
2023-01-12 15:40:53 +0900

    ci: Initial hello file for CI

---
+++ src/main/java/skku/stockplatform/StockPlatformApplication.java
@@ -0,0 +1,13 @@
+package skku.stockplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPlatformApplication.class, args);
+	}
+
+}
